af2aa57faf65e9dd3acaf3ad00d713576382f63c
kurumkan <joomrise@gmail.com>
2019-09-10 23:06:55 +0200

    add multiplicator

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -1,9 +1,29 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
-
 // Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+		@2 //sum
+		M=0 // sum = 0
+		@0 // R0
+		D=M // D=R0
+		@4 // i
+		M=D // i = R0
+(LOOP)	
+		// decrementing 
+		@4 // i
+		M=M-1 // i--	
 
-// Put your code here.
+		// break condition
+		D=M	
+		@END
+		D;JLT // if(i < 0) break;
+
+		// calculating sum	
+		@1 // R1
+		D=M // D = R1
+		@2  // sum
+		M=M + D // sum=sum+R1
+
+		
+		@LOOP
+		0;JMP // goto beginning of loop
+(END)
+		@END
+		0;JMP		
