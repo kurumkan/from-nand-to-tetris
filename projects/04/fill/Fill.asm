39330173a59ce884f3dfad44c5735ecd022e8048
kurumkan <joomrise@gmail.com>
2019-09-13 02:07:27 +0200

    implement Fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,8 +1,3 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
@@ -10,5 +5,55 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
+(BEGIN)
+	@SCREEN
+	D=A
+	@i 
+	M=D // i = 16384 (screens memory map base address)
 
-// Put your code here.
+	@8192 // number of 16 bit words from screen memory map
+	D=A
+	@SCREEN
+	D=D+A
+	@n
+	M=D // n = 16384 + 8192 
+
+(LOOP)	
+	@i
+	D=M
+	@n
+	D=D-M
+	@BEGIN
+	D;JGE // if(i > n) goto END
+	
+	@KBD // keyboard memory map address
+	D=M
+	@SETCOLOR
+	D;JNE // if(MEM[KBD] != 0) set color else no color (white)
+
+	@color
+	M=0
+	@PAINT
+	0;JMP
+
+(SETCOLOR)		
+	@color
+	M=-1
+
+(PAINT)
+	@color
+	D=M
+	@i
+	A=M
+	M=D // paint 
+
+	@i
+	M=M+1 // i++
+
+	@LOOP
+	0;JMP
+
+(END)
+	@END
+	0;JMP
+
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -1,29 +1,29 @@
 // Multiplies R0 and R1 and stores the result in R2.
-		@2 //sum
-		M=0 // sum = 0
-		@0 // R0
-		D=M // D=R0
-		@4 // i
-		M=D // i = R0
+	@R2 //sum
+	M=0 // sum = 0
+	@R0 // R0
+	D=M // D=R0
+	@R4 // i
+	M=D // i = R0
 (LOOP)	
-		// decrementing 
-		@4 // i
-		M=M-1 // i--	
+	// decrementing 
+	@R4 // i
+	M=M-1 // i--	
 
-		// break condition
-		D=M	
-		@END
-		D;JLT // if(i < 0) break;
+	// break condition
+	D=M	
+	@END
+	D;JLT // if(i < 0) break;
 
-		// calculating sum	
-		@1 // R1
-		D=M // D = R1
-		@2  // sum
-		M=M + D // sum=sum+R1
+	// calculating sum	
+	@R1 // R1
+	D=M // D = R1
+	@R2  // sum
+	M=M + D // sum=sum+R1
 
-		
-		@LOOP
-		0;JMP // goto beginning of loop
+	
+	@LOOP
+	0;JMP // goto beginning of loop
 (END)
-		@END
-		0;JMP		
+	@END
+	0;JMP		
