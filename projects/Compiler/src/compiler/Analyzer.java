e81242d0146e0866ce13facda3ba00243b8f9a0d
kurumkan <joomrise@gmail.com>
2019-11-14 21:14:25 +0100

    handle directory input

--- projects/Compiler/src/compiler/Analyzer.java
+++ projects/Compiler/src/compiler/Analyzer.java
@@ -9,34 +9,35 @@ public class Analyzer {
             String inputPath = args[0].trim();
             File src = new File(inputPath);
             CompilationEngine engine;  
-            Tokenizer tokenizer;
-            BufferedWriter writer;
+            Tokenizer tokenizer;            
                     
             if(src.isDirectory()) {
                 if(!new File(inputPath, "Main.jack").exists()) {
                     throw new Exception("Folder must contain Main.jack file");
                 }
                 
-                String outputPath = (inputPath.endsWith("/") ? inputPath : inputPath + "/")  + src.getName() + ".xml";
-
-                writer = new BufferedWriter(new FileWriter(outputPath));                
+                BufferedWriter writer;
+                
                 File[] files = src.listFiles((d, fileName) -> fileName.endsWith(".jack"));
                         
-                for(int i = 0; i < files.length; i += 1) {
+                for(int i = 0; i < files.length; i += 1) {                    
                     File file = files[i];                    
-                    parser = new Parser(file);
-                    writer.setFileName(file.getName());
-                    processCommands(parser, writer);
+                    tokenizer = new Tokenizer(file);         
+                    String filePath = file.getAbsolutePath();
+                    String outputPath = filePath.substring(0, filePath.length() - 5) + ".xml";;
+                    writer = new BufferedWriter(new FileWriter(outputPath));                      
+                    engine = new CompilationEngine(tokenizer, writer);                
+                    engine.compileClass();                    
+                    writer.close();
                 }
             } else {
                 String outputPath = inputPath.substring(0, inputPath.length() - 5) + ".xml";
                 tokenizer = new Tokenizer(src);                
-                writer = new BufferedWriter(new FileWriter(outputPath));    
-                
+                BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath)); 
                 engine = new CompilationEngine(tokenizer, writer);                
-                engine.compileClass();
-            }           
-            writer.close();
+                engine.compileClass();                
+                writer.close();
+            }                       
         } catch(Exception e) {
             e.printStackTrace();
         }
