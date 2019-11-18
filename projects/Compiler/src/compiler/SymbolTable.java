fd733a0ac319b4cca2708d6e122cc6148fceeea2
kurumkan <joomrise@gmail.com>
2019-11-19 00:01:47 +0100

    add SymbolTable

---
+++ projects/Compiler/src/compiler/SymbolTable.java
@@ -0,0 +1,103 @@
+package compiler;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class SymbolTable {
+    private HashMap<String, SymbolTableElement> classScope;
+    private HashMap<String, SymbolTableElement> subroutineScope;
+    
+    public SymbolTable() {
+        classScope = new HashMap<String, SymbolTableElement>();
+        subroutineScope = new HashMap<String, SymbolTableElement>();
+    }
+    public void startSubroutine() {
+        subroutineScope = new HashMap<String, SymbolTableElement>();
+    }
+    public void define(String name, String type, String kind) throws Exception {
+        HashMap<String, SymbolTableElement> scope;
+        switch(kind) {
+            case "STATIC":
+            case "FIELD": {
+                scope = classScope;
+                break;
+            }             
+            case "ARG":
+            case "VAR": {
+                scope = subroutineScope;
+                break;
+            }            
+            default: {
+                throw new Exception("Unknown kind: " + kind);
+            }
+        }
+        int index = varCount(kind);
+        scope.put(name, new SymbolTableElement(type, kind, index));
+    }
+    public int varCount(String kind) throws Exception {
+        HashMap<String, SymbolTableElement> scope;
+        switch(kind) {
+            case "STATIC":
+            case "FIELD": {
+                scope = classScope;
+                break;
+            }
+            case "ARG":
+            case "VAR": {
+                scope = subroutineScope;
+                break;
+            }            
+            default: {
+                throw new Exception("Unknown kind: " + kind);
+            }
+        }
+        
+        int counter = 0;
+        for (Entry<String, SymbolTableElement> current : scope.entrySet()) {
+            if(current.getValue().getKind().equals(kind)) {
+                counter += 1;
+            }                    
+        }
+        
+        return counter;                
+    }
+    public String kindOf(String name) {
+        SymbolTableElement el = classScope.get(name);
+        
+        if(el == null) {
+            el = subroutineScope.get(name);
+        }
+        
+        if(el == null) {
+            return null;
+        }
+        
+        return el.getKind();
+    }
+    public String typeOf(String name) {
+         SymbolTableElement el = classScope.get(name);
+        
+        if(el == null) {
+            el = subroutineScope.get(name);
+        }
+        
+        if(el == null) {
+            return null;
+        }
+        
+        return el.getType();
+    }
+    public int indexOf(String name) {
+        SymbolTableElement el = classScope.get(name);
+        
+        if(el == null) {
+            el = subroutineScope.get(name);
+        }
+        
+        if(el == null) {
+            return -1;
+        }
+        
+        return el.getIndex();
+    }
+}
---
+++ projects/Compiler/src/compiler/SymbolTableElement.java
@@ -0,0 +1,25 @@
+package compiler;
+
+public class SymbolTableElement {
+    private String kind;
+    private String type;
+    private int index;
+    
+    public SymbolTableElement(String type, String kind, int index) {
+        this.kind = kind;
+        this.type = type;
+        this.index = index;        
+    }
+    
+    public String getKind() {
+        return kind;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public int getIndex() {
+        return index;
+    }
+}
